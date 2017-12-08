3f24158fc978e959f9c69c143b78d8484b815c11
Peter Burns <peterburnshicks@gmail.com>
2017-12-07 19:57:25 -0500

    added card styling and favorite button on click

--- assets/javascript/pets.js
+++ assets/javascript/pets.js
@@ -32,8 +32,21 @@ $.ajax({
     var card = $('<div class="card teal"></div>');
     var petCard = $('<div class="card-image"></div>');
     petCard.append($('<img src = "' + petRef.media.photos.photo[2].$t + '" />'));
-    petCard.append($('<span class="card-title">' + petRef.name.$t + '</span>'));
-    $(card).append(petCard);
-    card.append($('<div class="card-content white-text">' + petRef.description.$t + '</div>'));
+    petCard.append($('<a class="btn-floating btn-large halfway-fab waves-effect waves-light red favButton"><i class="material-icons">favorite_border</i></a>'));
+    card.append(petCard);
+    card.append($());
+    card.append($('<div class="card-content white-text">' + '<span class="white-text card-title">' + petRef.name.$t + '</span>' + petRef.description.$t + '</div>'));
+    card.append($('<div class="card-action"><a href="#">Link1</a><a href="#">Link2</a></div>'));
     $('#cards').append(card);
+});
+$('body').on('click','.favButton',function(){
+    if(user){
+        var name = $(this).parent().parent().children('.card-content').children('.card-title').text();
+        var description = $(this).parent().parent().children('.card-content').text();
+        $(this).children(0).text('favorite');
+        console.log(name,description);
+    }
+    else{
+        alert('have to be logged in to save favorites');
+    }
 });
