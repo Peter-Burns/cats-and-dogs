ea19e3af22466cfbd8a626025e6c83bd6ebdcee2
Peter Burns <peterburnshicks@gmail.com>
2017-12-10 14:47:19 -0500

    added favorites display to userpage

--- assets/javascript/userpage.js
+++ assets/javascript/userpage.js
@@ -1,5 +1,20 @@
-if (localUser) {
-    userRef.on('child_added', function (snap) {
-        
-    });
-}
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        userRef.on('child_added', function (snap) {
+            cardRef = snap.val();
+            var card = $('<div class="card teal"></div>');
+            card.attr('data-email', cardRef.email);
+            card.attr('data-shelterId', cardRef.shelterId);
+            var petCard = $('<div class="card-image"></div>');
+            petCard.append($('<img src = "' + cardRef.imgSrc + '" />'));
+            petCard.append($('<a class="btn-floating btn-large halfway-fab waves-effect waves-light red favButton"><i class="material-icons">favorite</i></a>'));
+            card.append(petCard);
+            card.append($('<div class="card-content white-text">' + '<span class="white-text card-title">' + cardRef.name + '</span>' + '<p class="desc">' + cardRef.description + '</p>' + '</div>'));
+            card.append($('<div class="card-action"><a class="mapLink modal-trigger" href="#modal1"><i class="material-icons">location_on</i>Shelter</a><a class="emailLink" href="mailto:' + cardRef.email + '"><i class="material-icons">mail</i> Email</a></div>'));
+            $('#cards').append(card);
+        });
+    }
+    else {
+        $('#cards').empty();
+    }
+});
