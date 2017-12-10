0044923b279ba485efa5983c7b6ca09d161c4274
Peter Burns <peterburnshicks@gmail.com>
2017-12-10 16:53:09 -0500

    added check in petCardBuilder for favorite button

--- assets/javascript/petCard.js
+++ assets/javascript/petCard.js
@@ -3,10 +3,14 @@ function petCardBuilder(petRef) {
     var card = $('<div class="card teal petCard"></div>');
     card.attr('data-email', petRef.email);
     card.attr('data-shelterId', petRef.shelterId);
-    if(petRef.key)card.attr('data-key', petRef.key);
+    var icon = 'favorite_border';
+    if(petRef.key){
+        card.attr('data-key', petRef.key);
+        icon = 'favorite';
+    }
     var petCard = $('<div class="card-image"></div>');
     petCard.append($('<img src = "' + petRef.imgSrc + '" />'));
-    petCard.append($('<a class="btn-floating btn-large halfway-fab waves-effect waves-light red favButton"><i class="material-icons">favorite_border    </i></a>'));
+    petCard.append($('<a class="btn-floating btn-large halfway-fab waves-effect waves-light red favButton"><i class="material-icons">' + icon + '</i></a>'));
     card.append(petCard);
     card.append($('<div class="card-content left-align white-text">' + '<span class="white-text card-title">' + petRef.name + '</span>' + '<p class="desc">' + petRef.description + '</p>' + '</div>'));
     card.append($('<div class="card-action"><a class="mapLink modal-trigger" href="#modal1"><i class="material-icons">location_on</i>Shelter</a><a class="emailLink" href="mailto:' + petRef.email + '"><i class="material-icons">mail</i> Email</a></div>'));
