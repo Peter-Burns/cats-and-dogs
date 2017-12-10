8dbcefd0d5bb7ad08fc30331a0430bbab31c3317
Peter Burns <peterburnshicks@gmail.com>
2017-12-10 13:21:53 -0500

    added drop downs to the adopt and dog pages

--- assets/javascript/breedList.js
+++ assets/javascript/breedList.js
@@ -1,18 +1,30 @@
-var queryUrl = 'https://api.petfinder.com/breed.list';
-var apiKey = 'fff64394dcb68ac0d534ca0aa808bd69';
-$.ajax({
-    url: queryUrl,
-    method: 'GET',
-    crossDomain: true,
-    dataType: 'jsonp',
-    data: {
-        key: apiKey,
-        animal: 'dog',
-        format: 'json'
+$('#animal').on('change', function () {
+    if ($(this).val()) {
+        var queryUrl = 'https://api.petfinder.com/breed.list';
+        var apiKey = 'fff64394dcb68ac0d534ca0aa808bd69';
+        var animal = $(this).val();
+        $.ajax({
+            url: queryUrl,
+            method: 'GET',
+            crossDomain: true,
+            dataType: 'jsonp',
+            data: {
+                key: apiKey,
+                animal: animal,
+                format: 'json'
+            }
+        }).done(function (response) {
+            var breeds = response.petfinder.breeds.breed;
+            $('#breed').empty();
+            $('#breed').append('<option value="">No Preference</option>');
+            for (var i = 0; i < breeds.length; i++) {
+                $('#breed').append($('<option>' + breeds[i].$t + '</option>'));
+            }
+            $('#breed').material_select();
+        });
     }
-}).done(function (response){
-    var breeds = response.petfinder.breeds.breed;
-    for(var i =0; i < breeds.length; i++){
-        $('#dropdownBreed').append($('<option>' + breeds[i].$t + '</option>'));
+    else{
+        $('#breed').empty();
+        $('#breed').material_select();
     }
 });
