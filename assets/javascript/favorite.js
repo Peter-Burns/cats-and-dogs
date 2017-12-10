f6e501c77b8569cbb09639753f1287e7db8016a2
Peter Burns <peterburnshicks@gmail.com>
2017-12-10 15:42:57 -0500

    added favorite remove functionality

--- assets/javascript/favorite.js
+++ assets/javascript/favorite.js
@@ -1,19 +1,27 @@
 $('body').on('click', '.favButton', function () {
     if (localUser) {
-        var name = $(this).parent().parent().children('.card-content').children('.card-title').text();
-        var description = $(this).parent().parent().children('.card-content').children('.desc').text();
-        var shelterId = $(this).parent().parent().attr('data-shelterId');
-        var email = $(this).parent().parent().attr('data-email');
-        var imgSrc = $(this).parent().children('img').attr('src');
-        $(this).children(0).text('favorite');
-        console.log(name, description, shelterId, email);
-        userRef.push({
-            imgSrc: imgSrc,
-            name: name,
-            description: description,
-            shelterId: shelterId,
-            email: email,
-        });
+        if ($(this).children(0).text() == 'favorite_border') {
+            var name = $(this).parent().parent().children('.card-content').children('.card-title').text();
+            var description = $(this).parent().parent().children('.card-content').children('.desc').text();
+            var shelterId = $(this).parent().parent().attr('data-shelterId');
+            var email = $(this).parent().parent().attr('data-email');
+            var imgSrc = $(this).parent().children('img').attr('src');
+            $(this).children(0).text('favorite');
+            console.log(name, description, shelterId, email);
+            var key = userRef.push({
+                imgSrc: imgSrc,
+                name: name,
+                description: description,
+                shelterId: shelterId,
+                email: email,
+            }); 
+            key = key.toString().split('/').pop();
+            $(this).parent().parent().attr('data-key',key);
+        }
+        else{
+            $(this).children(0).text('favorite_border');
+            userRef.child($(this).parent().parent().attr('data-key')).remove();
+        }
     }
     else {
         alert('have to be logged in to save favorites');
