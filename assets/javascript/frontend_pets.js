bad2d2aa4fb8b9edb70697484312fb2915cf3777
unknown <mkap25@yahoo.com>
2017-12-12 03:20:51 -0500

    added cool picture cycle pic feature

--- assets/javascript/frontend_pets.js
+++ assets/javascript/frontend_pets.js
@@ -2,3 +2,15 @@ $(document).ready(function () {
       $('.parallax').parallax();
       $(".button-collapse").sideNav();
 });
+
+var images = ['assets/images/cat_s.jpg', 'assets/images/dog_stare.jpg', 'assets/images/cat_s2.jpg', 'assets/images/dog.png'],
+index=0,
+maxImages= images.length - 1;
+var timer= setInterval(function(){
+	var currentImage= images[index];
+	index= (index == maxImages) ? 0 : ++index;
+	$('#banner').delay(2000).fadeOut(2000, function(){
+		$('#banner').attr("src", currentImage);
+		$('#banner').fadeIn(3000);
+	});
+}, 3000);
