d97f88166c311590069b19e783ed98b8828ddfec
Peter Burns <peterburnshicks@gmail.com>
2017-12-13 02:18:58 -0500

    added async handling of petCardBuilder to check if animal is already saved

--- assets/javascript/petCard.js
+++ assets/javascript/petCard.js
@@ -1,23 +1,30 @@
 function petCardBuilder(petRef) {
+    var icon = 'favorite_border';
     var card = $('<div class="card teal petCard"></div>');
     card.attr('data-email', petRef.email);
     card.attr('data-shelterId', petRef.shelterId);
     card.attr('data-id', petRef.id);
     card.attr('data-animal', petRef.animal);
-    var icon = 'favorite_border';
-    if (userRef) {
-        var ref = userRef.child(petRef.id);
-        ref.once("value").then(function(snapshot){
-            if(snapshot.exists()){
-                icon='favorite';
-            }
-        });
-    }
     var petCard = $('<div class="card-image"></div>');
     petCard.append($('<img class = "petPic" src = "' + petRef.imgSrc + '" />'));
-    petCard.append($('<a class="btn-floating btn-large halfway-fab waves-effect waves-light red favButton"><i class="material-icons">' + icon + '</i></a>'));
-    card.append(petCard);
-    card.append($('<div class="card-content left-align white-text">' + '<span class="white-text card-title">' + petRef.name + '</span>' + '<p class="desc">' + petRef.description + '</p>' + '</div>'));
-    card.append($('<div class="card-action"><a class="mapLink modal-trigger" href="#modal1"><i class="material-icons">location_on</i>Shelter</a><a class="emailLink" href="mailto:' + petRef.email + '"><i class="material-icons">mail</i> Email</a></div>'));
-    return card;
+    if (userRef) {
+        var ref = userRef.child(petRef.id);
+        ref.once("value").then(function (snapshot) {
+            if (snapshot.exists()) {
+                icon = 'favorite';
+            }
+            petCard.append($('<a class="btn-floating btn-large halfway-fab waves-effect waves-light red favButton"><i class="material-icons">' + icon + '</i></a>'));
+            card.append(petCard);
+            card.append($('<div class="card-content left-align white-text">' + '<span class="white-text card-title">' + petRef.name + '</span>' + '<p class="desc">' + petRef.description + '</p>' + '</div>'));
+            card.append($('<div class="card-action"><a class="mapLink modal-trigger" href="#modal1"><i class="material-icons">location_on</i>Shelter</a><a class="emailLink" href="mailto:' + petRef.email + '"><i class="material-icons">mail</i> Email</a></div>'));
+            return card;
+        });
+    }
+    else {
+        petCard.append($('<a class="btn-floating btn-large halfway-fab waves-effect waves-light red favButton"><i class="material-icons">' + icon + '</i></a>'));
+        card.append(petCard);
+        card.append($('<div class="card-content left-align white-text">' + '<span class="white-text card-title">' + petRef.name + '</span>' + '<p class="desc">' + petRef.description + '</p>' + '</div>'));
+        card.append($('<div class="card-action"><a class="mapLink modal-trigger" href="#modal1"><i class="material-icons">location_on</i>Shelter</a><a class="emailLink" href="mailto:' + petRef.email + '"><i class="material-icons">mail</i> Email</a></div>'));
+        return card;
+    }
 }
