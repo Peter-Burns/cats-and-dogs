0826719b98bf0f44372f491212d26aad6c0b6894
Peter Burns <peterburnshicks@gmail.com>
2017-12-13 03:52:10 -0500

    removed columns from card-action to improve mobile layout

--- assets/javascript/petCard.js
+++ assets/javascript/petCard.js
@@ -13,6 +13,6 @@ function petCardBuilder(petRef) {
     petCard.append($('<a class="btn-floating btn-large halfway-fab waves-effect waves-light red favButton"><i class="material-icons">' + icon + '</i></a>'));
     card.append(petCard);
     card.append($('<div class="card-content left-align white-text">' + '<span class="white-text card-title">' + petRef.name + '</span>' + '<p class="desc">' + petRef.description + '</p>' + '</div>'));
-    card.append($('<div class="card-action"><div class="row"><div class="col s4"><button class="btn"><a class="mapLink modal-trigger" href="#modal1"><i class="material-icons left">location_on</i>Shelter</a></button></div><div class="col s4"><button class="btn"><a class="emailLink" href="mailto:' + petRef.email + '"><i class="material-icons left">mail</i> Email</a></button></div><div class="col s4"><button class="btn"><a class="facebookLink"><i class="material-icons left">share</i>Share</a></button></div></div></div>'));
+    card.append($('<div class="card-action"><div class="row"><button class="btn cardBtn"><a class="mapLink modal-trigger" href="#modal1"><i class="material-icons left">location_on</i>Shelter</a></button><button class="btn"><a class="emailLink" href="mailto:' + petRef.email + '"><i class="material-icons left">mail</i> Email</a></button><button class="btn"><a class="facebookLink"><i class="material-icons left">share</i>Share</a></button></div></div>'));
     return card;
 }
